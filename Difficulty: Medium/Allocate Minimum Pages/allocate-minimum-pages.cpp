c7c3c666240f5880ba3b7cdba1d92de67c2d476f
Debojyoti Dev <debojyoti.devx@gmail.com>
2025-10-23 10:13:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
@@ -1,44 +1,46 @@
 class Solution {
   public:
-    bool ispossible(int maxnoofpages,vector <int> arr,int k){
-        int numofpeople = 1;
-        int N = arr.size();
-        long long sum = 0;
-        for(int i=0;i<N;i++){
-            sum += arr[i];
-            if(sum>maxnoofpages){
-                numofpeople++;
-                if(numofpeople>k){
+  
+    bool ispossible(int maxpages,vector <int> &arr,int k){
+        int numofstudents = 1;
+        long long totalpages = 0;
+        
+        for(int i=0;i<arr.size();i++){
+            totalpages += arr[i];
+            if(totalpages > maxpages){
+                numofstudents++;
+                if(numofstudents>k){
                     return false;
                 }
-                sum = arr[i];
+                totalpages = arr[i];
             }
         }
         return true;
     }
     int findPages(vector<int> &arr, int k) {
         // code here
+        
         int N = arr.size();
+        int st = *(max_element(arr.begin(),arr.end()));
+        long long end = accumulate(arr.begin(),arr.end(),0);
+        long long ans;
         
         if(k>N){
             return -1;
-        }else{
-            int st = *(max_element(arr.begin(),arr.end()));
-            long long end = accumulate(arr.begin(),arr.end(),0);
-            long long ans;
-            
-            while(st<=end){
-                
-                int mid = st + (end-st)/2;
-                
-                if(ispossible(mid,arr,k)){
-                    ans = mid;
-                    end = mid-1;
-                }else{
-                    st = mid+1;
-                }
-            }
-            return ans;
         }
+        
+        while(st<=end){
+            
+            long long mid = st + (end-st)/2;
+            
+            if(ispossible(mid,arr,k)){
+                ans = mid;
+                end = mid-1;
+            }else{
+                st = mid+1;
+            }
+        }
+        
+        return (int)ans;
     }
 };
