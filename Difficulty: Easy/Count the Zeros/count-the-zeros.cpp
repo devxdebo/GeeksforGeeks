40b53d5dbe8d3b5e130be76f6d704b653cd848da
Debojyoti Dev <debojyoti.devx@gmail.com>
2025-10-23 09:14:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count the Zeros/count-the-zeros.cpp
+++ Difficulty: Easy/Count the Zeros/count-the-zeros.cpp
@@ -4,42 +4,42 @@ class Solution {
   public:
     int countZeroes(vector<int> &arr) {
         // code here
+        
         int N = arr.size();
         int st = 0;
         int end = N-1;
-        int startpos = -1;
+        int firstpos = -1;
         
         while(st<=end){
             
             int mid = st + (end-st)/2;
             
             if(arr[mid]==0){
-                startpos = mid;
+                firstpos = mid;
                 end = mid-1;
             }else{
                 st = mid+1;
             }
         }
-        
-        if(startpos == -1){
+        if(firstpos == -1){
             return 0;
         }else{
             
+            int lastpos = -1;
             st = 0;
             end = N-1;
-            int endpos = -1;
             
             while(st<=end){
-            
                 int mid = st + (end-st)/2;
                 
                 if(arr[mid]==0){
-                    endpos = mid;
+                    lastpos = mid;
+                    
                 }
                 st = mid+1;
             }
             
-            return endpos - startpos + 1;
+            return lastpos-firstpos+1;
         }
     }
 };
