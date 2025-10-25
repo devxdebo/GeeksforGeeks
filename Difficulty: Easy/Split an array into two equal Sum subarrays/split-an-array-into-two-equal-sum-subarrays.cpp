1ff05680bda36fa80f486f7d9d65de0f9facee35
Debojyoti Dev <debojyoti.devx@gmail.com>
2025-10-25 10:51:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Split an array into two equal Sum subarrays/split-an-array-into-two-equal-sum-subarrays.cpp
+++ Difficulty: Easy/Split an array into two equal Sum subarrays/split-an-array-into-two-equal-sum-subarrays.cpp
@@ -3,34 +3,30 @@ class Solution {
     bool canSplit(vector<int>& arr) {
         // code here
         int N = arr.size();
-        int st = 0;
-        int end = N-1;
         
-        if(N==1){
-            return false;
-        }else{
-            int sum1 = arr[st];
-            int sum2 = arr[end];
-            
-            while(st<end){
-                
-                if(sum1>sum2){
-                    end--;
-                    if(end!=st)
-                        sum2 += arr[end];
-                }else{
-                    st++;
-                    if(end!=st)
-                        sum1 += arr[st];
-                }
-            }
-            
-            if(sum1==sum2){
-                return true;
-            }else{
-                return false;
-            }
+        vector <int> prefix(N,0);
+        vector <int> suffix(N,0);
+        
+        prefix[0] = arr[0];
+        suffix[N-1] = arr[N-1];
+        
+        for(int i=1;i<N;i++){
+            prefix[i] = arr[i]+prefix[i-1];
         }
         
+        for(int i=N-2;i>=0;i--){
+            suffix[i] = arr[i]+suffix[i+1];
+        }
+        
+        if(prefix[N-2]==suffix[N-1]){
+            return true;
+        }else{
+            for(int i=0;i<N-1;i++){
+                if(prefix[i]==suffix[i+1]){
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 };
