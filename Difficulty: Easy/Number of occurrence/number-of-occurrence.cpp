cbe0df140884b1edec71bd6c392c094ad34ef402
Debojyoti Dev <debojyoti.devx@gmail.com>
2025-10-30 09:12:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -1,20 +1,20 @@
 class Solution {
   public:
-    int countFreq(vector<int>& arr, int target) {
+    int countFreq(vector<int>& nums, int target) {
         // code here
-        int firstpos = -1;
-        int N = arr.size();
+        int N = nums.size();
         int st = 0;
         int end = N-1;
+        int firstpos = -1;
         
         while(st<=end){
             
             int mid = st + (end-st)/2;
             
-            if(arr[mid]==target){
+            if(nums[mid]==target){
                 firstpos = mid;
                 end = mid-1;
-            }else if(arr[mid]>target){
+            }else if(nums[mid]>target){
                 end = mid-1;
             }else{
                 st = mid+1;
@@ -33,17 +33,18 @@ class Solution {
                 
                 int mid = st + (end-st)/2;
                 
-                if(arr[mid]==target){
+                if(nums[mid]==target){
                     lastpos = mid;
                     st = mid+1;
-                }else if(arr[mid]>target){
+                }else if(nums[mid]>target){
                     end = mid-1;
                 }else{
                     st = mid+1;
                 }
             }
             
-            return lastpos - firstpos + 1;
+            return lastpos-firstpos+1;
         }
+        
     }
 };
