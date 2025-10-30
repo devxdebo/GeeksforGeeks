8f425725c880c879b189658f64e2def968097cb6
Debojyoti Dev <debojyoti.devx@gmail.com>
2025-10-30 11:17:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
+++ Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
@@ -1,31 +1,31 @@
 class Solution {
   public:
-    bool ispossible(int maxpages,vector <int> &arr,int k){
-        int numofstudents = 1;
-        long long totalpages = 0;
+    bool ispossible(int maxtime,vector <int> &arr, int k){
+        long long sum = 0;
+        int numofpainter = 1;
         
         for(int i=0;i<arr.size();i++){
-            totalpages += arr[i];
-            if(totalpages > maxpages){
-                numofstudents++;
-                if(numofstudents>k){
+            sum += arr[i];
+            
+            if(sum>maxtime){
+                numofpainter++;
+                if(numofpainter>k){
                     return false;
                 }
-                totalpages = arr[i];
+                sum = arr[i];
             }
         }
         return true;
     }
-    
     int minTime(vector<int>& arr, int k) {
         // code here
         int st = *(max_element(arr.begin(),arr.end()));
-        long long end = accumulate(arr.begin(),arr.end(),0);
-        long long ans;
+        int end = accumulate(arr.begin(),arr.end(),0);
+        int ans;
         
         while(st<=end){
             
-            long long mid = st + (end-st)/2;
+            int mid = st + (end-st)/2;
             
             if(ispossible(mid,arr,k)){
                 ans = mid;
@@ -34,7 +34,6 @@ class Solution {
                 st = mid+1;
             }
         }
-        
-        return (int)ans;
+        return ans;
     }
 };
